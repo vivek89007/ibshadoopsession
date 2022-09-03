0255cc7569c561167affefb51eb432121a246ea7
vivek89007 <vivek89007@gmail.com>
2022-09-03 18:12:06 +0530

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dataflair.hd.wc;
+package com.vk.wc;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
