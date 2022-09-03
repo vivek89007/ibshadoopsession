638122a95bd0009c670efc1d874f5e419ba6f82e
vivek89007 <vivek89007@gmail.com>
2022-09-03 16:08:30 +0530

    Add files via upload

---
+++ IntSumReducer.java
@@ -0,0 +1,26 @@
+package com.dataflair.hd.wc;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class IntSumReducer extends Reducer <Text, IntWritable, Text, IntWritable>
+{
+	private IntWritable result = new IntWritable();
+
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+	{
+//		Hi	[1 1 1 1 1 1....]
+		
+		int sum = 0;
+		for (IntWritable val : values)
+		{
+			sum += val.get();
+		}
+		result.set(sum);
+		context.write(key, result);
+	}
+}
